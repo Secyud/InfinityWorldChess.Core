db3ddb4ec63663f486677c3053b14be743d7a5f2
老土 <69134017+Secyud@users.noreply.github.com>
2023-06-28 17:11:08 +0800

    :sparkles: update format

--- InfinityWorldChess.Shared/Ugf/PairStruct.cs
+++ InfinityWorldChess.Shared/Ugf/PairStruct.cs
@@ -1,8 +1,9 @@
-﻿namespace InfinityWorldChess.Ugf;
-
-public struct PairStruct<T1,T2>
+﻿namespace InfinityWorldChess.Ugf
 {
-    public T1 First;
-    public T2 Second;
+    public struct PairStruct<T1,T2>
+    {
+        public T1 First;
+        public T2 Second;
 
+    }
 }
