31cf94abdb86a882affcb19aad1c605753369f36
老土 <69134017+Secyud@users.noreply.github.com>
2023-09-24 09:30:01 +0800

    update skillPlay

---
+++ BasicPackage/SkillDomain/SkillPlayDomain/NormalPlay.cs
@@ -0,0 +1,7 @@
+namespace InfinityWorldChess.SkillDomain.SkillPlayDomain
+{
+    public class NormalPlay:SkillPlay
+    {
+        
+    }
+}
---
+++ BasicPackage/SkillDomain/SkillPlayDomain/SkillPlayContainer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Ugf;
+using Secyud.Ugf;
+using Secyud.Ugf.Archiving;
+using Secyud.Ugf.AssetComponents;
+using Secyud.Ugf.AssetLoading;
+
+namespace InfinityWorldChess.SkillDomain.SkillPlayDomain
+{
+    public class SkillPlayContainer : AssetContainer<SkillPlay>
+    {
+        public new static AssetContainer<SkillPlay> Create(
+            IAssetLoader loader, string assetName)
+        {
+            return assetName.IsNullOrEmpty()
+                ? null
+                : new SkillPlayContainer
+                {
+                    Loader = loader,
+                    AssetName = "SkillPlay/" + assetName + ".prefab"
+                };
+        }
+
+        public new static AssetContainer<SkillPlay> Create(
+            Type loaderType, string assetName)
+        {
+            return Create(U.Get(loaderType) as IAssetLoader, assetName);
+        }
+
+        public new static AssetContainer<SkillPlay> Create<TAssetLoader>(
+            string assetName) where TAssetLoader : class, IAssetLoader
+        {
+            return Create(U.Get<TAssetLoader>(), assetName);
+        }
+
+
+        public override void Save(IArchiveWriter writer)
+        {
+            writer.WriteNullable(Loader);
+            writer.Write(AssetName);
+        }
+
+        public override void Load(IArchiveReader reader)
+        {
+            Loader = reader.ReadNullable<IAssetLoader>();
+            AssetName = reader.ReadString();
+        }
+    }
+}
